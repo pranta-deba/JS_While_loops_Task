7118fcc3a4ffecf3d283dbb4035a8a89d2c0269a
Pranto Deb <rajprit0m@outlook.com>
2024-01-29 21:18:10 +0600

    4th task OK

---
+++ task4.js
@@ -0,0 +1,12 @@
+/***
+
+As Ersa is learning now, she wants to explore more and more. Tell Ersa to generate a multiplication table for number 5
+
+ */
+
+let i = 1;
+while (i <= 10) {
+    let result = 5 * i;
+    console.log("5 * " + i + " = " + result);
+    i++;
+}
