b76314ae9add9b93b15d2e28897ebdc1f3a6cbf0
Pranto Deb <rajprit0m@outlook.com>
2024-01-29 20:55:32 +0600

    1st task OK

---
+++ task1.js
@@ -0,0 +1,13 @@
+/***
+
+"I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
+
+ */
+
+let massage = "I will invest at least 6 hrs every single day for next 60 days!";
+let count = 1;
+
+while (count <= 60) {
+    console.log('Prinf -',count, "-" ,massage);
+    count++;
+}
